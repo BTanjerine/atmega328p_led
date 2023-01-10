c9082c6a40da4fc8bc25189a801d0ba346329f0b
bTanjerine <braydenhtanji@gmail.com>
2023-01-10 00:33:08 -0800

    added usart functionality and print h

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-default: 
+default: build 
+	
+build: 
 	# compile c code 
-	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o first.o first.c
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o first.o first.c   
 	# convert to binary
 	avr-gcc -o first.bin first.o
+
+burn: 	build 
 	# convert to hex for arduino board
 	avr-objcopy -O ihex -R .eeprom first.bin first.hex
 	#send the hex file to the rom on the board
--- first.c
+++ first.c
@@ -1,22 +1,50 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(){
-	//set port B5 as output
-	DDRD = DDRD | (1 << DDD3);	//use bit shift to switch the 3rd bit of the direction register for port D
+#define FOSC 1843200
+#define BAUD 9600
+#define MYUBRR (F_CPU/16/BAUD)-1
 
+int main(int argc, char** argv){
+	/*
+	 * initialize usar
+	 */
+	//set baud rate
+	UBRR0H = (unsigned char) (MYUBRR>>8);
+	UBRR0L = (unsigned char) MYUBRR;
+
+	//enable receiver and transmitter
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+	//set frameformat (type of message)
+	UCSR0C = (1 << USBS0) | (1 << UCSZ00) | (1 << UCSZ01);
+
+
+	/*
+	 * set port B5 as output
+	 */
+	DDRD |= (1 << DDD3);
+
+	//print h
 
 	while(1){
-		//turn on portB5
-		PORTD = PORTD | (1 << PORTD3);	//use bitshift to turn 3rd bit of portd on
+		//keep waiting till the UDREn bit of UCSRnA reg is on
+		while(!(UCSR0A & (1 << UDRE0))); 
 		
+		UDR0 = 'h';	//transmit h char
+
+		//turn on portB5
+		PORTD |= (1 << PORTD3);	//turn on 3rd bit of portd register to turn led on
+
 		//wait
 		_delay_ms(1000); //1 sec wait
 
 		//turn off portB5
-		PORTD = PORTD & ~(1 << PORTD3);
 
+		PORTD ^= (1 << PORTD3);	//turn off 3rd bit of portd register to turn led off (1 xor 1 = 0 turn off) 
+		
 		//wait
 		_delay_ms(1000);
 	}
 }
+
